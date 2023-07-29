a4447bf9eef5d4a2a735f3e90ec57b83c94faf73
root <alexjosuegallardocandia2020@gmail.com>
2023-07-29 00:29:39 -0400

    Pruebas de CRUDS, errores con los modelos y relaciones

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::group(['prefix'=>'api'],function(){
+    Route::apiResource('/marcas','MarcaController');
+    Route::apiResource('/medidas','MedidaController');
+    Route::apiResource('/categorias','CategoriaController');
+    Route::apiResource('/articulos','ArticuloController');
+// });
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::apiResource('/marcas','MarcaController');
-Route::apiResource('/medidas','MedidaController');
-Route::apiResource('/categorias','CategoriaController');
-Route::apiResource('/articulos','ArticuloController');
+
